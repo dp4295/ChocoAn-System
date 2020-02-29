aedcc7f86886df14add9501c1a3c0273d92a9e48
James Bao <34732378+jambao24@users.noreply.github.com>
2020-02-29 15:38:06 -0800

    Update Person.java

--- ChocoAn-System/src/com/chocoan_system/Person.java
+++ ChocoAn-System/src/com/chocoan_system/Person.java
@@ -12,14 +12,20 @@ public class Person {
   protected int zip;
   protected Person next;
   
+  public final const int name_size = 25;
+  public final const int ID_size = 9;
+  public final const int street_size = 25;
+  public final const int city_size = 14;
+  public final const int zip_size = 5;
+  
   //protected Scanner input;
   public Person() {
-    this.name = "";
-    this.ID = 000000000;
-    this.street = "";
-    this.city = "";
+    this.name = null;
+    this.ID = null;
+    this.street = null;
+    this.city = null;
     this.state = "--";
-    this.zip = 00000;
+    this.zip = null;
     this.next = null;
   }
   
