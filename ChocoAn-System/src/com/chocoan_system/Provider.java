8e32bf0b5d66bc16ce6b8b5e06a63264ca1b712e
James Bao <34732378+jambao24@users.noreply.github.com>
2020-02-29 15:28:03 -0800

    Update Provider.java

--- ChocoAn-System/src/com/chocoan_system/Provider.java
+++ ChocoAn-System/src/com/chocoan_system/Provider.java
@@ -3,7 +3,11 @@ package com.chocoan_system;
 //import java.util.Scanner;
 
 public class Provider extends Person {
-
+  protected String date;
+  protected String time;
+  protected String service_type;
+  
+  public final double unit_fee = 69.69; // let's hard code the service fee for testing purposes first; this might belong in a different class
 
 
 }
