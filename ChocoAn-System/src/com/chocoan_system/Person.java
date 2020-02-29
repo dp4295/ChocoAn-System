679e69ce8f4c771c94cda03d8a02df2c467bb2d4
James Bao <34732378+jambao24@users.noreply.github.com>
2020-02-29 15:19:21 -0800

    Update Person.java
    added variables

--- ChocoAn-System/src/com/chocoan_system/Person.java
+++ ChocoAn-System/src/com/chocoan_system/Person.java
@@ -4,6 +4,21 @@ package com.chocoan_system;
 
 public class Person {
 
-
-
+  protected String name;
+  protected int ID;
+  protected String street;
+  protected String city;
+  protected String state;
+  protected int zip;
+  
+  //protected Scanner input;
+  public Person() {
+    this.name = "";
+    this.ID = 000000000;
+    this.street = "";
+    this.city = "";
+    this.state = "--";
+    this.zip = 00000;
+  }
+  
 }
