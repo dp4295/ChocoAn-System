baa063b08b5a380123c1344eddf7800a31c62b6d
James Bao <34732378+jambao24@users.noreply.github.com>
2020-03-01 16:23:31 -0800

    Update readFile.java

--- ChocoAn-System/src/com/chocoan_system/readFile.java
+++ ChocoAn-System/src/com/chocoan_system/readFile.java
@@ -3,7 +3,8 @@ package com.chocoan_system;
 public class readFile {
   
   // I'm guessing this class is where you read in file input and output the contents to the data structures?
-  // protected Provider yanHealer;
-  // protected Member darikaTheAddict;
+  // protected Provider darikaHealer;
+  // protected Member yanTheAddict;
   // protected History_node temp;
+  // Scanner input; //???
 }
