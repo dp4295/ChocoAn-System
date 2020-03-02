7ea1929a901382fad5d8d6895e677c6baf7a2eab
irisn <iris.ngarden@gmail.com>
2020-03-01 17:24:54 -0800

    adding arr node

---
+++ ChocoAn-System/src/com/chocoan_system/ARR_node.java
@@ -0,0 +1,18 @@
+package com.chocoan_system;
+
+public class ARR_node {
+
+    protected ARR_node()
+    {
+        int type = 0;
+        Person obj = null;
+        ARR_node next = null;
+
+    }
+
+    protected ARR_node(int type, Person obj)
+    {
+        int type = type;
+        Person obj = obj;
+    }
+}
