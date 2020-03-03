b0ee992a045f6af47abf3419ca8e9880b71fb0f1
Chris <fallenchris@live.com>
2020-03-02 18:10:39 -0800

    Added functions to ARR, still needs work

--- ChocoAn-System/src/com/chocoan_system/ARR.java
+++ ChocoAn-System/src/com/chocoan_system/ARR.java
@@ -4,13 +4,11 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class ARR
-{
+public class ARR {
     protected ARR_node[] head = new ARR_node[2];
-    public ARR()
-    {
-        for(int i = 0; i < 2; ++i)
-        {
+
+    public ARR() {
+        for (int i = 0; i < 2; ++i) {
             this.head[i] = null;
         }
     }
@@ -18,48 +16,59 @@ public class ARR
     //Base case, create node if it doesn't exist. If it does, call recursive function.
     //The flag is the choice the user makes when adding a member or provider.
     //Members will be stored in index 0, providers will be stored in index 1.
-    protected int createNode(int flag)
-    {
-        if(this.head[flag] == null)
-        {
+    protected int createNode(int flag) {
+        if (this.head[flag] == null) {
             this.head[flag] = new ARR_node();
             this.head[flag].create(flag);
             return 1;
-        }
+        } else {
 
-        else
-            return createNode(this.head[flag], flag);
+            ARR_node temp = new ARR_node();
+            temp.create(flag);
+            return createNode(this.head[flag], temp);
+        }
     }
 
-    //Recursive function, inserts into the list in order of last name.
-    protected int createNode(ARR_node head, int type)
-    {
-       if(head.getNext() == null)
-       {
-           ARR_node temp = new ARR_node();
-           temp.create(type);
-           head.setNext(temp);
-           temp.setNext(null);
-           return 2;
-       }
+    int compare(ARR_node head, ARR_node temp) {
+        return temp.obj.name.compareTo(head.obj.name);
+    }
 
-        String str1 = temp.obj.name;
-        String str2 = head.obj.name;
+
+    //Recursive function, inserts into the list in order of last name?????????.
+    protected int createNode(ARR_node head, ARR_node temp) {
+        //Add at the end of the list, no smaller names found
+        if (head.getNext() == null) {
+            head.setNext(temp);
+            return 2;
+        }
+
+        createNode(head.getNext(), temp);
 
         //If names are equal, check ID number, if ID number is the same return failure
         //If ID number is different, sort by ID number
-        if(str1.compareTo(str2) == 0 ) {
-
-            ARR_node temp = new ARR_node();
-            temp.create(type);
+        if (compare(head, temp) == 0) {
+            if (head.obj.ID == temp.obj.ID) {
+                return 0;
+            } else if (head.obj.ID > temp.obj.ID) {
+                temp.setNext(head);
+                return 2;
+            } else {
+                head.setNext(temp);
+                return 2;
+            }
         }
+
+        //If current name is less then temp name insert before
+        if (compare(head, temp) < 0) {
+            temp.setNext(head);
+            return 2;
+        }
+
+        //If current name is greater then temp name insert after
+        if (compare(head, temp) > 0) {
+            head.setNext(temp);
+            return 2;
+        }
+        return 0;
     }
-
-
-
-
-
-
-
-
 }
