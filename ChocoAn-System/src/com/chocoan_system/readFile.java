ed65dfe3b189caede6c8fedaceaadb3338805ba9
rither <rither@pdx.edu>
2020-03-07 15:18:28 -0800

    added new text files for member, servide codes and history

--- ChocoAn-System/src/com/chocoan_system/readFile.java
+++ ChocoAn-System/src/com/chocoan_system/readFile.java
@@ -3,13 +3,16 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+//readfile looks at file input
+//tests should be for file input
 public class readFile {
 
   // I'm guessing this class is where you read in file input and output the contents to the data structures?
-  // protected Provider ptemp;
-  // protected Member mtemp;
-  // protected History_node temp;
-  // Scanner input; //???
+  protected Provider provider;
+  protected Member member;
+  protected History_node history;
+  // Scanner input; //???1604
+
 
   public static void main(String[] args) {
     try {
@@ -18,7 +21,17 @@ public class readFile {
       //scanner.useDelimiter("|");
       while (scanner.hasNextLine()) {
         String data = scanner.nextLine();
-        System.out.println(data);//replace with code to store values
+        System.out.println(data);//replace with code to store values(how are we storing?)
+        provider.name = data;
+        provider.id = data;
+        provider.street = data;
+        provider.city = data;
+        provider.state = data;
+        provider.zip = data;
+
+
+
+
       }
       scanner.close();
     } catch (FileNotFoundException e) {
