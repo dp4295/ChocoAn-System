064fbef6d35a3931d0407209621d96a7cb4e9c84
rither <rither@pdx.edu>
2020-03-07 20:18:58 -0800

    added to text files

--- ChocoAn-System/src/com/chocoan_system/Member.java
+++ ChocoAn-System/src/com/chocoan_system/Member.java
@@ -1,7 +1,7 @@
 package com.chocoan_system;
 // Derived class of Person
 //import java.util.Scanner;
-
+/*
 public class Member extends Person {
   // protected String date;
  //  protected String time;
@@ -61,6 +61,9 @@ public class Member extends Person {
       add_service(myHead.get_Next(), temp2);
     }
   }
+  */
+
+public class Member extends Person {
 
 
 
