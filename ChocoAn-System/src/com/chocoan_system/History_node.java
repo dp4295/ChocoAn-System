9abc0aa9fefdabaa6c20c91fcf2d560d332fc6ce
irisn <iris.ngarden@gmail.com>
2020-03-07 16:08:51 -0800

    updated history node file

--- ChocoAn-System/src/com/chocoan_system/History_node.java
+++ ChocoAn-System/src/com/chocoan_system/History_node.java
@@ -2,21 +2,28 @@ package com.chocoan_system;
 // Class for storing data about a treatment session
 //import java.util.Scanner;
 
-public class History_node {
+
+    public class History_node {
   protected String date;
   protected String time;
-  protected String service_type;
+  protected String service_date;
+  protected String service_code;
+  protected String prov_id;
+  protected String mem_id;
   protected String comments;
   protected History_node next;
   //public final double unit_fee = 69.69; // let's hard code the service fee for testing purposes first; this might belong in a different class
+//blsh blah
 
-  
   public History_node() {
     this.date = "__-__-____";
     this.time = "__:__";
-    this.service_type = null;
+    this.service_date = "__-__-____";
+    this.service_code = null;
+    this.prov_id = null;
+    this.mem_id = null;
     this.comments = null;
     this.next = null;
   }
-  
+
 }
