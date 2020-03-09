9719b19ed6fa1ca5b2b5fc71ff6798aee0fa8077
irisn <iris.ngarden@gmail.com>
2020-03-08 18:18:11 -0700

    updated member java file

--- ChocoAn-System/src/com/chocoan_system/Member.java
+++ ChocoAn-System/src/com/chocoan_system/Member.java
@@ -4,69 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
-// Derived class of Person
-//import java.util.Scanner;
-/*
-public class Member extends Person {
-  // protected String date;
- //  protected String time;
-  protected History_node head;
-
-  
-  public final double unit_fee = 69.69; // let's hard code the service fee for testing purposes first; this might belong in a different class
-
-  
-  public Member() {
-   // this.date = null;
-    // this.time = null;
-    this.head = null;
-
-  }
-
-
-  // Constructor with argument list
-  public Member(String name, int ID, String street, String city, String state, int zip, String date, String time)
-  {
-    super(name, ID, street, city, state, zip);
-    // this.date = date;
-    // this.time = time;
-    this.head = null;
-  }
-
-
-  public void build()
-  {
-     super.build();
-
-  }
-
-  public void display()
-  {
-    System.out.println("\n\n");
-    System.out.println("\tMember name: "+ name);
-    System.out.println("\tMember Id:" + ID);
-    System.out.println("\tStreet: " + street);
-    System.out.println("\tState: " + state);
-    System.out.println("\tZip code: "+ zip);
-    System.out.println("\n");
-  }
-
-  // adding a new History_node to the list of History_nodes for the member
-  public void add_service(History_node temp)
-  {
-    add_service(head, temp);
-  }
-  // recursive helper function for adding History_node to the LLL
-  private void add_service(History_node myHead, History_node temp2) {
-    if (myHead == null) {
-      myHead = new History_node();
-      // need to populate the newly created History_node object with the contents of temp2
-    }
-    else {
-      add_service(myHead.get_Next(), temp2);
-    }
-  }
-  */
 
 public class Member {
 
@@ -90,19 +27,21 @@ public class Member {
             System.out.println("Error occurred while creating a file");
             e.printStackTrace();
         }
-    }
 
-
-
-        writeout_member_directory() {
+        public void writeout_member_directory() {
 
 
         }
 
-        writeout_member_reports() {
+        public void writeout_member_reports() {
 
         }
 
 
     }
+
+
+
+
+
 }
